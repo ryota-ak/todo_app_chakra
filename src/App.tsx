4843e2ff297716fe79db91b677290b0d7945f6d6
ryota-ak <72099017+ryota-ak@users.noreply.github.com>
2021-07-22 02:53:23 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { Box, Flex, Heading } from "@chakra-ui/layout";
+import { VFC } from "react";
+import Header from "./component/Header";
+import TaskInput from "./features/task/TaskInput";
+import TaskList from "./features/task/TaskList";
 
-function App() {
+const App: VFC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <Box bg="gray.100" w="100vw" h="100vh">
+        <Box w={800} mx="auto" py={10}>
+          <Flex direction="column">
+            <Header />
+            <TaskInput />
+            <TaskList />
+          </Flex>
+        </Box>
+      </Box>
+    </>
   );
-}
+};
 
 export default App;
